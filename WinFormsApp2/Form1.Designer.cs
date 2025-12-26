af5dbdfb23cda5b474be76a5a035a124cdbbb416
Doğukan Baycan <dogukan.bycnn34@gmail.com>
2025-12-27 00:54:22 +0300

    Add files via upload

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            kullaniciadi = new TextBox();
+            sifre = new TextBox();
+            mail = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            mailsifre = new TextBox();
+            kategori = new ComboBox();
+            label6 = new Label();
+            listView1 = new ListView();
+            koplaya = new Button();
+            sil = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(42, 231);
+            button1.Name = "button1";
+            button1.Size = new Size(236, 82);
+            button1.TabIndex = 0;
+            button1.Text = "Hesap Ekle";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // kullaniciadi
+            // 
+            kullaniciadi.Location = new Point(12, 35);
+            kullaniciadi.Name = "kullaniciadi";
+            kullaniciadi.Size = new Size(149, 27);
+            kullaniciadi.TabIndex = 1;
+            kullaniciadi.TextChanged += textBox1_TextChanged;
+            // 
+            // sifre
+            // 
+            sifre.Location = new Point(197, 35);
+            sifre.Name = "sifre";
+            sifre.Size = new Size(149, 27);
+            sifre.TabIndex = 2;
+            // 
+            // mail
+            // 
+            mail.Location = new Point(12, 101);
+            mail.Name = "mail";
+            mail.Size = new Size(149, 27);
+            mail.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Kullanıcı adı";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(197, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Şifre";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 78);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Mail";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(197, 78);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 20);
+            label4.TabIndex = 10;
+            label4.Text = "Mail Şifre";
+            // 
+            // mailsifre
+            // 
+            mailsifre.Location = new Point(197, 101);
+            mailsifre.Name = "mailsifre";
+            mailsifre.Size = new Size(149, 27);
+            mailsifre.TabIndex = 4;
+            // 
+            // kategori
+            // 
+            kategori.FormattingEnabled = true;
+            kategori.Items.AddRange(new object[] { "Instagram", "Tiktok", "Twitter", "Facebook", "Youtube" });
+            kategori.Location = new Point(12, 173);
+            kategori.Name = "kategori";
+            kategori.Size = new Size(151, 28);
+            kategori.TabIndex = 13;
+            kategori.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 150);
+            label6.Name = "label6";
+            label6.Size = new Size(149, 20);
+            label6.TabIndex = 14;
+            label6.Text = "Hesap Kategorisi Seç";
+            label6.Click += label6_Click;
+            // 
+            // listView1
+            // 
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Location = new Point(389, 12);
+            listView1.Name = "listView1";
+            listView1.ShowGroups = false;
+            listView1.Size = new Size(560, 325);
+            listView1.TabIndex = 15;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // koplaya
+            // 
+            koplaya.Location = new Point(955, 12);
+            koplaya.Name = "koplaya";
+            koplaya.Size = new Size(162, 35);
+            koplaya.TabIndex = 16;
+            koplaya.Text = "Hesap Bilgi Kopyala";
+            koplaya.UseVisualStyleBackColor = true;
+            koplaya.Click += koplaya_Click;
+            // 
+            // sil
+            // 
+            sil.Location = new Point(955, 69);
+            sil.Name = "sil";
+            sil.Size = new Size(162, 35);
+            sil.TabIndex = 17;
+            sil.Text = "Hesap Bilgi Sil";
+            sil.UseVisualStyleBackColor = true;
+            sil.Click += sil_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1127, 356);
+            Controls.Add(sil);
+            Controls.Add(koplaya);
+            Controls.Add(listView1);
+            Controls.Add(label6);
+            Controls.Add(kategori);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(mailsifre);
+            Controls.Add(mail);
+            Controls.Add(sifre);
+            Controls.Add(kullaniciadi);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Hesap Kayıt Programı";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox kullaniciadi;
+        private TextBox sifre;
+        private TextBox mail;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox mailsifre;
+        private ComboBox kategori;
+        private Label label6;
+        private ListView listView1;
+        private Button koplaya;
+        private Button sil;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,259 @@
+﻿namespace WinFormsApp2
+
+{
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Collections;
+
+    public partial class Form1 : Form
+    {
+        Dictionary<string, int> kategoriOncelik = new Dictionary<string, int>()
+{
+    { "Instagram", 1 },
+    { "Tiktok", 2 },
+    { "Twitter", 3 },
+    { "Youtube", 4 },
+    { "Facebook", 5 }
+};
+
+
+
+
+        string dosyaYolu = "veriler.txt";
+
+        [DllImport("user32.dll")]
+        public static extern bool LockWorkStation();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (kullaniciadi.Text == "" || sifre.Text == "")
+            {
+                MessageBox.Show("Kullanıcı adı ve şifre boş olamaz");
+                return;
+            }
+
+            // Yeni item oluştur
+            ListViewItem yeniItem = new ListViewItem(kategori.Text);
+            yeniItem.SubItems.Add(kullaniciadi.Text);
+            yeniItem.SubItems.Add(sifre.Text);
+            yeniItem.SubItems.Add(mail.Text);
+            yeniItem.SubItems.Add(mailsifre.Text);
+
+            int eklenecekIndex = listView1.Items.Count; // varsayılan: sona ekle
+            int yeniKategoriOncelik = kategoriOncelik[kategori.Text];
+
+            // ListView içini dolaş
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                string mevcutKategori = listView1.Items[i].SubItems[0].Text;
+                int mevcutOncelik = kategoriOncelik[mevcutKategori];
+
+                // Aynı kategori varsa → onun ALTINA ekle
+                if (mevcutKategori == kategori.Text)
+                {
+                    eklenecekIndex = i + 1;
+                }
+                // Daha büyük öncelikli kategoriye gelince DUR
+                else if (mevcutOncelik > yeniKategoriOncelik)
+                {
+                    eklenecekIndex = i;
+                    break;
+                }
+            }
+
+            // Doğru yere ekle
+            listView1.Items.Insert(eklenecekIndex, yeniItem);
+
+            // DOSYAYA YAZ (sıra doğru)
+            string satir =
+                kategori.Text + "|" +
+                kullaniciadi.Text + "|" +
+                sifre.Text + "|" +
+                mail.Text + "|" +
+                mailsifre.Text;
+
+            File.AppendAllText(dosyaYolu, satir + Environment.NewLine);
+
+            // Temizle
+            kullaniciadi.Clear();
+            sifre.Clear();
+            mail.Clear();
+            mailsifre.Clear();
+            kategori.SelectedIndex = -1;
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.MultiSelect = true;
+            listView1.HideSelection = false;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Kategori", 120);
+            listView1.Columns.Add("Kullanıcı Adı", 120);
+            listView1.Columns.Add("Şifre", 70);
+            listView1.Columns.Add("Mail", 120);
+            listView1.Columns.Add("Mail Şifre", 120);
+
+
+            listView1.Groups.Add(new ListViewGroup("Instagram"));
+            listView1.Groups.Add(new ListViewGroup("Tiktok"));
+            listView1.Groups.Add(new ListViewGroup("Twitter"));
+            listView1.Groups.Add(new ListViewGroup("Facebook"));
+            listView1.Groups.Add(new ListViewGroup("Youtube"));
+
+            // DOSYA VARSA OKU
+            if (File.Exists(dosyaYolu))
+            {
+                string[] satirlar = File.ReadAllLines(dosyaYolu);
+
+                foreach (string satir in satirlar)
+                {
+                    string[] parca = satir.Split('|');
+
+                    if (parca.Length == 5)
+                    {
+                        ListViewItem item = new ListViewItem(parca[0]);
+                        item.SubItems.Add(parca[1]);
+                        item.SubItems.Add(parca[2]);
+                        item.SubItems.Add(parca[3]);
+                        item.SubItems.Add(parca[4]);
+
+                        int eklenecekIndex = listView1.Items.Count;
+                        int yeniKategoriOncelik = kategoriOncelik[parca[0]];
+
+                        for (int i = 0; i < listView1.Items.Count; i++)
+                        {
+                            string mevcutKategori = listView1.Items[i].SubItems[0].Text;
+                            int mevcutOncelik = kategoriOncelik[mevcutKategori];
+
+                            if (mevcutKategori == parca[0])
+                            {
+                                eklenecekIndex = i + 1;
+                            }
+                            else if (mevcutOncelik > yeniKategoriOncelik)
+                            {
+                                eklenecekIndex = i;
+                                break;
+                            }
+                        }
+
+                        listView1.Items.Insert(eklenecekIndex, item);
+                    }
+                }
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void koplaya_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Kopyalamak için en az bir kayıt seçmelisin.");
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                sb.AppendLine("Kullanıcı Adı: " + item.SubItems[0].Text);
+                sb.AppendLine("Şifre: " + item.SubItems[1].Text);
+                sb.AppendLine("Mail: " + item.SubItems[2].Text);
+                sb.AppendLine("Mail Şifre: " + item.SubItems[3].Text);
+                sb.AppendLine("Kategori: " + item.SubItems[4].Text);
+                sb.AppendLine("----------------------------");
+            }
+
+            Clipboard.SetText(sb.ToString());
+            MessageBox.Show("Seçilen kayıtlar kopyalandı.");
+        }
+
+        private void sil_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Silmek için en az bir kayıt seçmelisin.");
+                return;
+            }
+
+            DialogResult sonuc = MessageBox.Show(
+                "Seçili kayıtlar silinsin mi?",
+                "Onay",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (sonuc == DialogResult.Yes)
+            {
+                foreach (ListViewItem item in listView1.SelectedItems)
+                {
+                    listView1.Items.Remove(item);
+                }
+            }
+            // DOSYAYI BAŞTAN YAZ
+            StringBuilder sb = new StringBuilder();
+
+            foreach (ListViewItem item in listView1.Items)
+            {
+                sb.AppendLine(
+                    item.SubItems[0].Text + "|" +
+                    item.SubItems[1].Text + "|" +
+                    item.SubItems[2].Text + "|" +
+                    item.SubItems[3].Text + "|" +
+                    item.SubItems[4].Text
+                );
+            }
+
+            File.WriteAllText(dosyaYolu, sb.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+      
+    }
+
+
+  
+}
